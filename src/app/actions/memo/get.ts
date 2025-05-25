9a9de6caf0477bb5bac8f5f2364e426e3e605c3e
Tatsuhiko Sakaguchi <ta21cos@users.noreply.github.com>
2025-05-25 19:26:54 +0900

    chore: add debug log

--- src/app/actions/memo/get.ts
+++ src/app/actions/memo/get.ts
@@ -7,6 +7,28 @@ import { toSerializable } from './utils';
 import { MemoRepository } from '../../../lib/db';
 import { Memo } from '@/lib/prisma/types';
 
+import * as fs from 'fs';
+import * as path from 'path';
+
+const showFiles = (folderName: string) => {
+  const directoryPath = path.join(__dirname, folderName);
+
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+
+    console.log('Files in directory:');
+    files.forEach((file) => {
+      console.log(folderName, file);
+    });
+  });
+};
+
+showFiles('/var/task/node_modules/.prisma/client');
+showFiles('/vercel/path0/node_modules/@prisma/client');
+
 /**
  * Get all memos (root level, no parent)
  *
