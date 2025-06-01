97f048bf29049e5e6a1e1567691df0db62b168a8
Tatsuhiko Sakaguchi <ta21cos@users.noreply.github.com>
2025-06-01 14:41:11 +0900

    chore: specify generate directory

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/app/generated/prisma';
 
 // Prevent multiple instances of Prisma Client in development
 const globalForPrisma = global as unknown as {
