66829d665a5f26a14510bb063b505724d2f7f79a
Tatsuhiko Sakaguchi <ta21cos@users.noreply.github.com>
2025-06-01 14:45:38 +0900

    Revert "chore: specify generate directory"
    This reverts commit 97f048bf29049e5e6a1e1567691df0db62b168a8.

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@/app/generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 // Prevent multiple instances of Prisma Client in development
 const globalForPrisma = global as unknown as {
