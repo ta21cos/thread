90e0a8a4c562bd32f3ebdec0d2f2c5e7218d0dd5
Tatsuhiko Sakaguchi <ta21cos@users.noreply.github.com>
2025-06-01 14:52:52 +0900

    chore: remove unnecessary

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -13,7 +13,5 @@ export const prisma =
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-console.log({ prisma });
-
 // Store the Prisma Client instance in global scope in development
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
